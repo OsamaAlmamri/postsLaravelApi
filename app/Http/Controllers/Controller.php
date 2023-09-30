c79326fdef5914cfcfe972ab4d06f657895f7296
Osama Al-mamari <yemencoder@gmail.com>
2023-09-30 04:23:26 +0300

    developer info

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,13 +14,13 @@ use Illuminate\Routing\Controller as BaseController;
  *              "url": "https://via.placeholder.com/190x90.png?text=L5-Swagger"
  *          }
  *      },
- *      title="L5 OpenApi",
- *      description="L5 Swagger OpenApi description",
+ *      title="Osama Almamari (779558800)",
+ *      description="Posts Api Documntion",
  *      @OA\Contact(
- *          email="darius@matulionis.lt"
+ *          email="osama.moh.almamari@gmail.com"
  *      ),
  *     @OA\License(
- *         name="Apache 2.0",
+ *         name="Post api 1.0",
  *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
  *     )
  * )
